af6313feac20acc877318aa6bca921480c939a46
Jacob Ochoa <jacob.ochoa@gmail.com>
2022-05-18 16:49:01 -0700

    fix indent

--- fatal.go
+++ fatal.go
@@ -3,5 +3,5 @@ package want
 import "testing"
 
 func Fatal(t *testing.T, want, got interface{}) {
-	t.Fatalf("\nwant: %+v\n got %+v\n", want, got)
+	t.Fatalf("\nwant: %+v\ngot %+v\n", want, got)
 }
